0438494103dcc27e11a664819655e2c01134d6d1
tantingqiang <tantingqiang@viomi.com.cn>
2021-03-18 22:45:41 +0800

    feat: 新增构造函数代码

--- js/extend.js
+++ js/extend.js
@@ -1,14 +1,35 @@
 // 原型链继承
 
+// function Car () {
+//   this.name = 'car'
+// }
+
+// function aCar () {
+//   this.type = 'a'
+// }
+
+// aCar.prototype = new Car()
+// const fit = new aCar()
+// console.log(fit)
+// console.log(fit.name)
+// console.log(fit.hasOwnProperty('name'))
+
+// 构造函数继承
+
 function Car () {
-  this.name = 'car'
-}
-function aCar () {
-  this.type = 'a'
+  this.name = "car"
 }
 
-aCar.prototype = new Car()
+Car.prototype.getName = () => {
+  return this.name
+}
+
+function aCar () {
+  Car.call(this)
+  this.type = "a"
+}
+
 const fit = new aCar()
-console.log(fit)
-console.log(fit.name)
-console.log(fit.hasOwnProperty('name'))
+
+console.log(fit);
+console.log(fit.getName());
